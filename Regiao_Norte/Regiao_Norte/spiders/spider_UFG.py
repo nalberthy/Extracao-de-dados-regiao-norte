7d920e7ba74e0c90c3345a3edb5d2fb472c20963
nalberthy <nalberthyuf@gmail.com>
2019-05-17 12:08:05 -0300

    Spiders feitos, faltando tratamento de dados

--- Regiao_Norte/Regiao_Norte/spiders/spider_UFG.py
+++ Regiao_Norte/Regiao_Norte/spiders/spider_UFG.py
@@ -11,22 +11,32 @@ class SpiderUfgSpider(scrapy.Spider):
         lista = response.css('td[headers = "t2"] a::attr(href)').extract()
         self.log(lista)
         #link de referência do botão next
-        # next_page = response.css('.pagination  li:last-child > a::attr(href)').get()
+        next_page = response.css('.pagination  li:last-child > a::attr(href)').get()
         # Percorrer artigos da pagina
         for link in lista:
             yield response.follow(link+"?mode=full", self.extracao)
         # Percorrer paginas    
-        # if next_page is not None:
-        #     next_page = response.urljoin(next_page)
-        #     yield scrapy.Request(next_page, callback=self.parse)        
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)        
     def extracao(self,response):
         itens = RegiaoNorteItem()
-        titulo = response.css('tr:nth-child(26)  td:nth-child(2) ::text').extract()
-        resumo= response.css('tr:nth-child(29) td:nth-child(2)::text').extract()
-        data = response.css('tr:nth-child(7) td:nth-child(2) ::text').extract()
-        self.log(resumo)
-        itens['titulo']=titulo
-        itens['resumo']=resumo
-        itens['data']=data
-        yield itens
+        campoDC  = response.css('td.metadataFieldLabel::text').extract()
+        valor  = response.css('td:nth-child(2)::text').extract()
+        try:
+            itens['titulo'] = valor[campoDC.index('dc.title')]
+        except Exception:
+            itens['titulo'] =[]
+        try:
+            itens['resumo'] = valor[campoDC.index('dc.description.resumo')]
+        except Exception:
+            try:
+                itens['resumo'] = valor[campoDC.index('dc.description.abstract')]
+            except Exception:
+                itens['resumo'] =[]
+        try:
+            itens['data'] = valor[campoDC.index('dc.date.issued')]
+        except Exception:
+            itens['data'] =[]
 
+        yield itens
